d8be85c9039fa6aaa79a7f635bfa84ea2023f49c
JATIN SRIVASTAVA <93035514+jatinsri25@users.noreply.github.com>
2024-07-09 02:47:28 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect("mongodb+srv://srivastavajatin9898:gIgyM3yo6gaQ7Uag@cluster0.ssq6js1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       dbName: "MERN_JOB_SEEKING_WEBAPP",
     })
     .then(() => {
